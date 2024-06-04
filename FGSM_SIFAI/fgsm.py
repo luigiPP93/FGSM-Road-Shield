547f64b0274d8d1e08542c202bbbf2ab711095e0
luigiPP93 <lavoroluigipp93@gmail.com>
2024-06-04 11:43:31 +0200

    Update fgsm.py

--- FGSM_SIFAI/fgsm.py
+++ FGSM_SIFAI/fgsm.py
@@ -16,6 +16,7 @@ import model_intrusion
 import tensorflow as tf
 import json
 from sklearn.metrics import confusion_matrix
+import numpy as np
 import requests
 from PIL import Image
 from keras.applications import VGG16
@@ -53,6 +54,18 @@ def readData():
     X_train, y_train = train_data['features'], train_data['labels']
     X_val, y_val = valid_data['features'], valid_data['labels']
     X_test, y_test = test_data['features'], test_data['labels']
+
+    print(X_train.shape)
+    print(X_val.shape)
+    print(X_test.shape)
+
+    assert(X_train.shape[0] == y_train.shape[0]), 'The number of images is not equal to the number of labels'
+    assert(X_val.shape[0] == y_val.shape[0]), 'The number of images is not equal to the number of labels'
+    assert(X_test.shape[0] == y_test.shape[0]), 'The number of images is not equal to the number of labels'
+
+    assert(X_train.shape[1:] == (32,32,3)), "The dimensions of the images are not 32x32x3"
+    assert(X_val.shape[1:] == (32,32,3)), "The dimensions of the images are not 32x32x3"
+    assert(X_test.shape[1:] == (32,32,3)), "The dimensions of the images are not 32x32x3"
 
     data = pd.read_csv('FGSM_SIFAI/german-traffic-signs/signnames.csv')
 
@@ -656,12 +669,50 @@ def display_gradcam(img_path, heatmap, alpha=0.3):
 if __name__ == '__main__':
     
     # Dictionary mapping class labels to their corresponding names
-    classes = {0: 'Speed limit (20km/h)',
-               1: 'Speed limit (30km/h)',
-               2: 'Speed limit (50km/h)',
-               # ... and so on ...
-               41: 'End of no passing',
-               42: 'End no passing veh > 3.5 tons'}
+    classes = { 0:'Speed limit (20km/h)',
+            1:'Speed limit (30km/h)',
+            2:'Speed limit (50km/h)',
+            3:'Speed limit (60km/h)',
+            4:'Speed limit (70km/h)',
+            5:'Speed limit (80km/h)',
+            6:'End of speed limit (80km/h)',
+            7:'Speed limit (100km/h)',
+            8:'Speed limit (120km/h)',
+            9:'No passing',
+            10:'No passing veh over 3.5 tons',
+            11:'Right-of-way at intersection',
+            12:'Priority road',
+            13:'Yield',
+            14:'Stop',
+            15:'No vehicles',
+            16:'Veh > 3.5 tons prohibited',
+            17:'No entry',
+            18:'General caution',
+            19:'Dangerous curve left',
+            20:'Dangerous curve right',
+            21:'Double curve',
+            22:'Bumpy road',
+            23:'Slippery road',
+            24:'Road narrows on the right',
+            25:'Road work',
+            26:'Traffic signals',
+            27:'Pedestrians',
+            28:'Children crossing',
+            29:'Bicycles crossing',
+            30:'Beware of ice/snow',
+            31:'Wild animals crossing',
+            32:'End speed + passing limits',
+            33:'Turn right ahead',
+            34:'Turn left ahead',
+            35:'Ahead only',
+            36:'Go straight or right',
+            37:'Go straight or left',
+            38:'Keep right',
+            39:'Keep left',
+            40:'Roundabout mandatory',
+            41:'End of no passing',
+            42:'End no passing veh > 3.5 tons' }
+    
     
     # Import necessary libraries and modules
     data, X_train, y_train, X_val, X_test, y_test, y_val = readData()
